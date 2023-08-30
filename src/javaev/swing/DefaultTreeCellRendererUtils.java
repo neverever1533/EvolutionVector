37c63adf153a0713ee2a71577248661674778005
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2023-08-30 22:04:03 +0800

    Update DefaultTreeCellRendererUtils.java

--- src/javaev/swing/DefaultTreeCellRendererUtils.java
+++ src/javaev/swing/DefaultTreeCellRendererUtils.java
@@ -1,113 +1,343 @@
-package javaev.swing;
-
-import java.awt.Component;
-import java.awt.geom.AffineTransform;
-import java.awt.image.BufferedImage;
-import java.awt.image.ImageObserver;
+package javaev.io;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
 
+import java.nio.charset.Charset;
+
+import java.util.Iterator;
 import java.util.List;
 
-import javax.swing.ImageIcon;
-import javax.swing.JTree;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeCellRenderer;
-
 import cn.imaginary.toolkit.image.ImageLayer;
+import javaev.awt.DimensionUtils;
+import javaev.awt.PointUtils;
+import javaev.awt.RectangleUtils;
 
-import javaev.awt.Graphics2DUtils;
+public class FileUtils {
+	private static FileUtils instance;
 
-import javaev.imageio.ImageIOUtils;
+	public static FileUtils getInstance() {
+		if (null == instance) {
+			syncInit();
+		}
+		return instance;
+	}
 
-import javaev.io.FileNameMapUtils;
-import javaev.io.FileUtils;
+	private static synchronized void syncInit() {
+		if (null == instance) {
+			instance = new FileUtils();
+		}
+	}
 
-public class DefaultTreeCellRendererUtils extends DefaultTreeCellRenderer {
-	/**
-	 * @author Sureness
-	 */
-	private static final long serialVersionUID = -22463755647610747L;
+	private String[] suffixArray;
 
-	private FileNameMapUtils fileNameMapUtils = new FileNameMapUtils();
+	private FileUtils() {
+	}
 
-	private FileUtils fileUtils = FileUtils.getInstance();
-
-	private Graphics2DUtils graphics2dUtils = new Graphics2DUtils();
-
-	private ImageIOUtils imageIOUtils = new ImageIOUtils();
-
-	private List<ImageLayer> layerListRoot;
-
-	private ImageIcon getImageIconScaled(BufferedImage image, double width, double height, float alpha,
-			ImageObserver observer) {
-		if (null != image) {
-			double w = image.getWidth();
-			double h = image.getHeight();
-			if (width <= 0) {
-				width = height * image.getWidth() / image.getHeight();// wr/w=hr/h
+	private boolean checkFileArray(File[] fileArray, boolean isDirectoryOnly) {
+		if (null == fileArray) {
+			return false;
+		}
+		File file;
+		for (int i = 0; i < fileArray.length; i++) {
+			file = fileArray[i];
+			if (isDirectoryOnly) {
+				if (!file.isDirectory()) {
+					return false;
+				}
+			} else {
+				if (!file.isFile()) {
+					return false;
+				}
 			}
-			if (height <= 0) {
-				height = width * image.getHeight() / image.getWidth();
-			}
-			AffineTransform xform = new AffineTransform();
-			xform.scale(width / w, height / h);
-			image = graphics2dUtils.filterImage(image, null, xform);
-			ImageIcon imageIcon = new ImageIcon();
-			imageIcon.setImage(image);
-			return imageIcon;
+		}
+		return true;
+	}
+
+	public String[] getCharsetArray() {
+		return (String[]) Charset.availableCharsets().keySet().toArray();
+	}
+
+	public String getContentType(File file) {
+		if (null != file) {
+			FileNameMapUtils fnmu = new FileNameMapUtils();
+			return fnmu.getContentTypeFor(file);
 		}
 		return null;
 	}
 
-	@Override
-	public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded, boolean leaf,
-			int row, boolean hasFocus) {
-		super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
-		DefaultMutableTreeNode treeNode;
-		if (null != value && value instanceof DefaultMutableTreeNode) {
-			treeNode = (DefaultMutableTreeNode) value;
-			if (null != treeNode) {
-				Object object = treeNode.getUserObject();
-				if (null != object) {
-					File file = null;
-					ImageIcon imageIcon = null;
-					int w = -1;
-					int h = 16;
-					float alpha = 1.0f;
-					ImageObserver observer = null;
-					ImageLayer imageLayer;
-					BufferedImage image;
-					if (object instanceof String) {
-						String pathName = (String) object;
-						imageLayer = fileUtils.getImageLayer(layerListRoot, pathName);
-						if (null != imageLayer) {
-							image = imageLayer.getImage();
-							imageIcon = getImageIconScaled(image, w, h, alpha, observer);
-						}
-					} else if (object instanceof File) {
-						file = (File) object;
-						if (fileNameMapUtils.isFileImage(file)) {
-							image = imageIOUtils.read(file);
-							imageIcon = getImageIconScaled(image, w, h, alpha, observer);
-						}
-					} else if (object instanceof ImageLayer) {
-						imageLayer = (ImageLayer) object;
-						image = imageLayer.getImage();
-						imageIcon = getImageIconScaled(image, w, h, alpha, observer);
-					} else {
-						imageIcon = (ImageIcon) this.getIcon();
-					}
-					if (null != imageIcon) {
-						this.setIcon(imageIcon);
+	public File getFile(File file, Object name, boolean isReplace, String suffix) {
+		if (null != file && null != suffix) {
+			File dir;
+			String fileName;
+			if (file.isDirectory()) {
+				dir = file;
+				fileName = dir.getName();
+			} else {
+				dir = file.getParentFile();
+				fileName = getFileNamePrefix(file);
+			}
+			StringBuffer sbuf = new StringBuffer();
+			if (null != dir && null != fileName) {
+				if (null == name || !isReplace) {
+					sbuf.append(fileName);
+				} else {
+					sbuf.append(name);
+				}
+				sbuf.append(suffix);
+				return new File(dir, sbuf.toString());
+			}
+		}
+		return null;
+	}
+
+	public File getFile(File file, Object name, String suffix) {
+		return getFile(file, name, false, suffix);
+	}
+
+	public File getFile(File file, String suffix) {
+		return getFile(file, null, suffix);
+	}
+
+	public File getFile(List<File> list, String fileName) {
+		if (null != list && null != fileName) {
+			File file;
+			for (Iterator<File> iterator = list.iterator(); iterator.hasNext();) {
+				file = (File) iterator.next();
+				if (null != file) {
+					if (file.getAbsolutePath().endsWith(fileName)) {
+						return file;
 					}
 				}
 			}
 		}
-		return this;
+		return null;
 	}
 
-	public void setResourcesList(List<ImageLayer> layerList) {
-		layerListRoot = layerList;
+	public FileFilter getFileFilter() {
+		return getFileFilter(getSuffixArray());
+	}
+
+	public FileFilter getFileFilter(final String[] suffixArray) {
+		if (null != suffixArray) {
+			FileFilter fileFilter = new FileFilter() {
+				public boolean accept(File pathname) {
+					String name = pathname.getName().toLowerCase();
+					String suffix;
+					for (int i = 0, iLength = suffixArray.length; i < iLength; i++) {
+						suffix = suffixArray[i];
+						if (pathname.isDirectory() || (null != suffix && name.endsWith(suffix))) {
+							return true;
+						}
+					}
+					return false;
+				}
+			};
+			return fileFilter;
+		}
+		return null;
+	}
+
+	private String getFileName(File file, boolean isSuffix) {
+		if (null != file) {
+			String fileName = file.getName();
+			int ids = fileName.lastIndexOf(".");
+			if (isSuffix) {
+				if (ids != -1) {
+					return fileName.substring(ids);
+				}
+			} else {
+				if (ids != -1) {
+					return fileName.substring(0, ids);
+				} else {
+					return fileName;
+				}
+			}
+		}
+		return null;
+	}
+
+	public String[] getFileNameArray(File[] fileArray) {
+		return toStringArray(fileArray, true);
+	}
+
+	public FilenameFilter getFileNameFilter() {
+		return getFileNameFilter(getSuffixArray());
+	}
+
+	public FilenameFilter getFileNameFilter(final String[] suffixArray) {
+		FilenameFilter fileFilter = new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				String fName = name.toLowerCase();
+				File file = new File(dir, name);
+				String temp;
+				for (int i = 0, iLength = suffixArray.length; i < iLength; i++) {
+					temp = suffixArray[i];
+					if (file.isDirectory() || (null != temp && fName.endsWith(temp))) {
+						return true;
+					}
+				}
+				return false;
+			}
+		};
+		return fileFilter;
+	}
+
+	public String getFileNamePrefix(File file) {
+		return getFileName(file, false);
+	}
+
+	public String getFileNameSuffix(File file) {
+		return getFileName(file, true);
+	}
+
+	public String[] getFilePathArray(File[] fileArray) {
+		return toStringArray(fileArray, false);
+	}
+
+	public File getFileReplace(File file, Object name, String suffix) {
+		return getFile(file, name, true, suffix);
+	}
+
+	public ImageLayer getImageLayer(List<ImageLayer> list, File file) {
+		if (null != file) {
+			return getImageLayer(list, file.getPath());
+		}
+		return null;
+	}
+
+	public ImageLayer getImageLayer(List<ImageLayer> layerList, int depth) {
+		if (null != layerList) {
+			ImageLayer imageLayer;
+			int depthLayer;
+			for (Iterator<ImageLayer> iterator = layerList.iterator(); iterator.hasNext();) {
+				imageLayer = iterator.next();
+				if (null != imageLayer) {
+					depthLayer = imageLayer.getDepth();
+					if (depthLayer == depth) {
+						return imageLayer;
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	public ImageLayer getImageLayer(List<ImageLayer> list, Object object) {
+		if (null != list && null != object) {
+			ImageLayer imageLayer;
+			Object objectLayer;
+			for (Iterator<ImageLayer> iterator = list.iterator(); iterator.hasNext();) {
+				imageLayer = iterator.next();
+				if (null != imageLayer) {
+					objectLayer = imageLayer.getObject();
+					if (null != objectLayer && objectLayer.equals(object)) {
+						return imageLayer;
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	public int getImageLayerDepthMax(List<ImageLayer> list) {
+		if (null != list) {
+			ImageLayer layer;
+			int depth;
+			int index = -1;
+			for (Iterator<ImageLayer> iterator = list.iterator(); iterator.hasNext();) {
+				layer = iterator.next();
+				if (null != layer) {
+					depth = layer.getDepth();
+					if (depth > index) {
+						index = depth;
+					}
+				}
+			}
+			return index;
+		}
+		return -1;
+	}
+
+	public RectangleUtils getImageLayersRectangleMin(List<ImageLayer> list) {
+		if (null != list) {
+			ImageLayer layer;
+			PointUtils p;
+			DimensionUtils d;
+			int r;
+			int b;
+			int right = 0;
+			int bottom = 0;
+			int left = -1;
+			int top = -1;
+			for (Iterator<ImageLayer> iterator = list.iterator(); iterator.hasNext();) {
+				layer = iterator.next();
+				if (null != layer && layer.isVisible()) {
+					p = layer.getLocation();
+					d = layer.getSize();
+					if (left < 0) {
+						left = p.x + d.width;
+					}
+					if (top < 0) {
+						top = p.y + d.height;
+					}
+					if (p.x < left) {
+						left = p.x;
+					}
+					if (p.y < top) {
+						top = p.y;
+					}
+					r = p.x + d.width;
+					if (r > right) {
+						right = r;
+					}
+					b = p.y + d.height;
+					if (b > bottom) {
+						bottom = b;
+					}
+				}
+			}
+			return new RectangleUtils(left, top, right - left, bottom - top);
+		}
+		return null;
+	}
+
+	public String[] getSuffixArray() {
+		return suffixArray;
+	}
+
+	public int indexOf(List<ImageLayer> list, String fileName) {
+		ImageLayer layer = getImageLayer(list, fileName);
+		if (null != layer) {
+			return list.indexOf(layer);
+		}
+		return -1;
+	}
+
+	public boolean isDirectoryArrayOnly(File[] fileArray) {
+		return checkFileArray(fileArray, true);
+	}
+
+	public boolean isFileArrayOnly(File[] fileArray) {
+		return checkFileArray(fileArray, false);
+	}
+
+	public void setSuffixArray(String[] array) {
+		suffixArray = array;
+	}
+
+	private String[] toStringArray(File[] fileArray, boolean isFileName) {
+		int len = fileArray.length;
+		String[] array = new String[len];
+		for (int i = 0, iLength = len; i < iLength; i++) {
+			File file = fileArray[i];
+			if (isFileName) {
+				array[i] = file.getName();
+			} else {
+				array[i] = file.getAbsolutePath();
+			}
+		}
+		return array;
 	}
 }
